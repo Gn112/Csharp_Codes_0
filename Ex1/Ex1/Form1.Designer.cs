e9f8bfe9186ba73de0fe2bcba62654b74de28635
Gabriel Siriani <50271636+Gn112@users.noreply.github.com>
2023-03-16 08:12:14 -0300

    Conversor de hh:mm:ss para apenas segundos

---
+++ Ex1/Ex1/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace Ex1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtHour = new System.Windows.Forms.TextBox();
+            this.txtSec = new System.Windows.Forms.TextBox();
+            this.txtMin = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblRes = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 25F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 41);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Hora:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 25F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(10, 134);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(198, 41);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Segundo:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Verdana", 25F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(12, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(162, 41);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Minuto:";
+            // 
+            // txtHour
+            // 
+            this.txtHour.BackColor = System.Drawing.Color.Indigo;
+            this.txtHour.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtHour.Font = new System.Drawing.Font("Verdana", 25F, System.Drawing.FontStyle.Bold);
+            this.txtHour.ForeColor = System.Drawing.Color.White;
+            this.txtHour.Location = new System.Drawing.Point(214, 40);
+            this.txtHour.Name = "txtHour";
+            this.txtHour.Size = new System.Drawing.Size(100, 41);
+            this.txtHour.TabIndex = 6;
+            this.txtHour.Text = "0";
+            // 
+            // txtSec
+            // 
+            this.txtSec.BackColor = System.Drawing.Color.Indigo;
+            this.txtSec.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtSec.Font = new System.Drawing.Font("Verdana", 25F, System.Drawing.FontStyle.Bold);
+            this.txtSec.ForeColor = System.Drawing.Color.White;
+            this.txtSec.Location = new System.Drawing.Point(214, 134);
+            this.txtSec.Name = "txtSec";
+            this.txtSec.Size = new System.Drawing.Size(100, 41);
+            this.txtSec.TabIndex = 7;
+            this.txtSec.Text = "0";
+            // 
+            // txtMin
+            // 
+            this.txtMin.BackColor = System.Drawing.Color.Indigo;
+            this.txtMin.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtMin.Font = new System.Drawing.Font("Verdana", 25F, System.Drawing.FontStyle.Bold);
+            this.txtMin.ForeColor = System.Drawing.Color.White;
+            this.txtMin.Location = new System.Drawing.Point(214, 87);
+            this.txtMin.Name = "txtMin";
+            this.txtMin.Size = new System.Drawing.Size(100, 41);
+            this.txtMin.TabIndex = 8;
+            this.txtMin.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Indigo;
+            this.button1.Font = new System.Drawing.Font("Verdana", 25F, System.Drawing.FontStyle.Bold);
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(17, 192);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(228, 59);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "CALCULAR";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Verdana", 20F, System.Drawing.FontStyle.Bold);
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(49, 278);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(173, 32);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Segundos:";
+            // 
+            // lblRes
+            // 
+            this.lblRes.AutoSize = true;
+            this.lblRes.Font = new System.Drawing.Font("Verdana", 20F, System.Drawing.FontStyle.Bold);
+            this.lblRes.ForeColor = System.Drawing.Color.White;
+            this.lblRes.Location = new System.Drawing.Point(111, 337);
+            this.lblRes.Name = "lblRes";
+            this.lblRes.Size = new System.Drawing.Size(33, 32);
+            this.lblRes.TabIndex = 11;
+            this.lblRes.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Indigo;
+            this.ClientSize = new System.Drawing.Size(272, 414);
+            this.Controls.Add(this.lblRes);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtMin);
+            this.Controls.Add(this.txtSec);
+            this.Controls.Add(this.txtHour);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Exercício 01";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtHour;
+        private System.Windows.Forms.TextBox txtSec;
+        private System.Windows.Forms.TextBox txtMin;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblRes;
+    }
+}
+
---
+++ Ex1/Ex1/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ex1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        int hora = 0;
+        int minuto = 0;
+        int segundo = 0;
+
+        int calculo = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            hora = int.Parse(txtHour.Text);
+            minuto = int.Parse(txtMin.Text);
+            segundo = int.Parse(txtSec.Text);
+
+            calculo = (hora * 3600) + (minuto * 60) + segundo;
+
+            lblRes.Text = calculo.ToString() + "s";
+        }
+    }
+}
