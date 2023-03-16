ccde6c726cd69459b79256aac1f92365a41ff6f5
Gabriel Siriani <50271636+Gn112@users.noreply.github.com>
2023-03-16 08:48:34 -0300

    Converter total dos produtos para Real.

---
+++ Ex2/Ex2/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Ex2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.txtQuant = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblTotalPago = new System.Windows.Forms.Label();
+            this.btnConverter = new System.Windows.Forms.Button();
+            this.lblTotalPagoUS = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Valor a Pagar em Dólar:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(62, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Quantidade:";
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(133, 48);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(100, 20);
+            this.txtValor.TabIndex = 2;
+            // 
+            // txtQuant
+            // 
+            this.txtQuant.Location = new System.Drawing.Point(133, 74);
+            this.txtQuant.Name = "txtQuant";
+            this.txtQuant.Size = new System.Drawing.Size(100, 20);
+            this.txtQuant.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(78, 155);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Total dos Produtos:";
+            // 
+            // lblTotalPago
+            // 
+            this.lblTotalPago.AutoSize = true;
+            this.lblTotalPago.Location = new System.Drawing.Point(98, 211);
+            this.lblTotalPago.Name = "lblTotalPago";
+            this.lblTotalPago.Size = new System.Drawing.Size(45, 13);
+            this.lblTotalPago.TabIndex = 5;
+            this.lblTotalPago.Text = "R$ 0,00";
+            // 
+            // btnConverter
+            // 
+            this.btnConverter.Location = new System.Drawing.Point(90, 112);
+            this.btnConverter.Name = "btnConverter";
+            this.btnConverter.Size = new System.Drawing.Size(75, 23);
+            this.btnConverter.TabIndex = 6;
+            this.btnConverter.Text = "button1";
+            this.btnConverter.UseVisualStyleBackColor = true;
+            this.btnConverter.Click += new System.EventHandler(this.btnConverter_Click);
+            // 
+            // lblTotalPagoUS
+            // 
+            this.lblTotalPagoUS.AutoSize = true;
+            this.lblTotalPagoUS.Location = new System.Drawing.Point(98, 186);
+            this.lblTotalPagoUS.Name = "lblTotalPagoUS";
+            this.lblTotalPagoUS.Size = new System.Drawing.Size(52, 13);
+            this.lblTotalPagoUS.TabIndex = 7;
+            this.lblTotalPagoUS.Text = "US$ 0,00";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(272, 409);
+            this.Controls.Add(this.lblTotalPagoUS);
+            this.Controls.Add(this.btnConverter);
+            this.Controls.Add(this.lblTotalPago);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtQuant);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.TextBox txtQuant;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblTotalPago;
+        private System.Windows.Forms.Button btnConverter;
+        private System.Windows.Forms.Label lblTotalPagoUS;
+    }
+}
+
---
+++ Ex2/Ex2/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ex2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        float preco;
+        int quantidade;
+        double totalDolar;
+        double totalReal;
+
+        private void btnConverter_Click(object sender, EventArgs e)
+        {
+            preco = float.Parse(txtValor.Text);
+            quantidade = int.Parse(txtQuant.Text);
+
+            totalDolar = (preco * quantidade);
+            totalReal = totalDolar * 5.29;
+
+            lblTotalPagoUS.Text = $"US$ {totalDolar.ToString()}";
+            lblTotalPago.Text = $"R$ {totalReal.ToString()}";
+        }
+
+    }
+}
