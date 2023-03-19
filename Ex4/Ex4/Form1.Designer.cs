1b3411156bf467e212f8625d1c98e0ab448e4ca1
Gabriel Siriani <50271636+Gn112@users.noreply.github.com>
2023-03-19 12:18:22 -0300

    Add files via upload

---
+++ Ex4/Ex4/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Ex4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtEntrada = new System.Windows.Forms.TextBox();
+            this.txtAtraso = new System.Windows.Forms.TextBox();
+            this.btnCal = new System.Windows.Forms.Button();
+            this.lblValorFinal = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Valor Entrada:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Dias Atraso:";
+            // 
+            // txtEntrada
+            // 
+            this.txtEntrada.Location = new System.Drawing.Point(15, 39);
+            this.txtEntrada.Name = "txtEntrada";
+            this.txtEntrada.Size = new System.Drawing.Size(100, 20);
+            this.txtEntrada.TabIndex = 2;
+            // 
+            // txtAtraso
+            // 
+            this.txtAtraso.Location = new System.Drawing.Point(15, 88);
+            this.txtAtraso.Name = "txtAtraso";
+            this.txtAtraso.Size = new System.Drawing.Size(100, 20);
+            this.txtAtraso.TabIndex = 3;
+            // 
+            // btnCal
+            // 
+            this.btnCal.Location = new System.Drawing.Point(26, 124);
+            this.btnCal.Name = "btnCal";
+            this.btnCal.Size = new System.Drawing.Size(75, 23);
+            this.btnCal.TabIndex = 4;
+            this.btnCal.Text = "Calcular";
+            this.btnCal.UseVisualStyleBackColor = true;
+            this.btnCal.Click += new System.EventHandler(this.btnCal_Click);
+            // 
+            // lblValorFinal
+            // 
+            this.lblValorFinal.AutoSize = true;
+            this.lblValorFinal.Location = new System.Drawing.Point(12, 184);
+            this.lblValorFinal.Name = "lblValorFinal";
+            this.lblValorFinal.Size = new System.Drawing.Size(59, 13);
+            this.lblValorFinal.TabIndex = 5;
+            this.lblValorFinal.Text = "Valor Final:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(149, 393);
+            this.Controls.Add(this.lblValorFinal);
+            this.Controls.Add(this.btnCal);
+            this.Controls.Add(this.txtAtraso);
+            this.Controls.Add(this.txtEntrada);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtEntrada;
+        private System.Windows.Forms.TextBox txtAtraso;
+        private System.Windows.Forms.Button btnCal;
+        private System.Windows.Forms.Label lblValorFinal;
+    }
+}
+
---
+++ Ex4/Ex4/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ex4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double entradaValor;
+        int diasAtraso;
+        double valorFinal;
+        double multa;
+
+        private void btnCal_Click(object sender, EventArgs e)
+        {
+            entradaValor = Convert.ToDouble(txtEntrada.Text);
+            diasAtraso = Convert.ToInt32(txtAtraso.Text);
+
+            multa = diasAtraso * (0.05 / 100);
+
+            valorFinal = entradaValor + (8.49) + multa;
+
+            lblValorFinal.Text = $"Valor Final: {valorFinal}";
+        }
+    }
+}
