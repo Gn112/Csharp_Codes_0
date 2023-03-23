989729799ca6f971149ba15f968763f68526e1a5
Gabriel Siriani <50271636+Gn112@users.noreply.github.com>
2023-03-23 08:40:05 -0300

    Add files via upload

---
+++ Ex4Pag15/Ex4Pag15/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace Ex4Pag15
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblOrdem = new System.Windows.Forms.Label();
+            this.btnOrdenar = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblOrdemReverse = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Numero 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Numero 2:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Numero 3:";
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(74, 17);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(100, 20);
+            this.txt1.TabIndex = 3;
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(74, 47);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(100, 20);
+            this.txt2.TabIndex = 4;
+            // 
+            // txt3
+            // 
+            this.txt3.Location = new System.Drawing.Point(74, 73);
+            this.txt3.Name = "txt3";
+            this.txt3.Size = new System.Drawing.Size(100, 20);
+            this.txt3.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(46, 146);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Ordem Crescente:";
+            // 
+            // lblOrdem
+            // 
+            this.lblOrdem.AutoSize = true;
+            this.lblOrdem.Location = new System.Drawing.Point(46, 191);
+            this.lblOrdem.Name = "lblOrdem";
+            this.lblOrdem.Size = new System.Drawing.Size(0, 13);
+            this.lblOrdem.TabIndex = 7;
+            // 
+            // btnOrdenar
+            // 
+            this.btnOrdenar.Location = new System.Drawing.Point(54, 107);
+            this.btnOrdenar.Name = "btnOrdenar";
+            this.btnOrdenar.Size = new System.Drawing.Size(75, 23);
+            this.btnOrdenar.TabIndex = 8;
+            this.btnOrdenar.Text = "Ordenar";
+            this.btnOrdenar.UseVisualStyleBackColor = true;
+            this.btnOrdenar.Click += new System.EventHandler(this.btnOrdenar_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(46, 239);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(105, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Ordem Decrescente:";
+            // 
+            // lblOrdemReverse
+            // 
+            this.lblOrdemReverse.AutoSize = true;
+            this.lblOrdemReverse.Location = new System.Drawing.Point(51, 277);
+            this.lblOrdemReverse.Name = "lblOrdemReverse";
+            this.lblOrdemReverse.Size = new System.Drawing.Size(0, 13);
+            this.lblOrdemReverse.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(192, 398);
+            this.Controls.Add(this.lblOrdemReverse);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnOrdenar);
+            this.Controls.Add(this.lblOrdem);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txt3);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.TextBox txt3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblOrdem;
+        private System.Windows.Forms.Button btnOrdenar;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblOrdemReverse;
+    }
+}
+
---
+++ Ex4Pag15/Ex4Pag15/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ex4Pag15
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void btnOrdenar_Click(object sender, EventArgs e)
+        {
+            double num1 = double.Parse(txt1.Text);
+            double num2 = double.Parse(txt2.Text);
+            double num3 = double.Parse(txt3.Text);
+
+            double[] ordem = new double[3] {num1, num2, num3};
+
+            Array.Sort(ordem);
+
+            foreach(double i in ordem)
+            {
+                lblOrdem.Text += " " + i;
+            }
+
+            Array.Reverse(ordem);
+
+            foreach (double i in ordem)
+            {
+                lblOrdemReverse.Text += " " + i;
+            }
+
+        }
+    }
+}
